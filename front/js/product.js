1033af1cf66107d6049e51b8264bad9f0ddfe9b8
Geneviève B <genevieve.bourchis@gmail.com>
2023-01-17 15:28:35 +0100

    informations fiches produit + début ajouter au panier

--- front/js/product.js
+++ front/js/product.js
@@ -14,87 +14,34 @@ fetch("http://localhost:3000/api/products/" + productId)
 .then((res) => res.json())
 .then((data) => {
     console.log(data)
-        let img = document.createElement('img')
-        img.src = data.imageUrl
-        img.alt = data.altTxt
-        document.getElementsByClassName('item__img')[0].appendChild(img)
-        description.innerText = data.item_img
-        document.getElementById('price').innerHTML = data.price
-        price.innerText = 'Prix :${data.price} €';
 
-    }
+   let img = document.createElement('img')
+    img.src = data.imageUrl
+    img.alt = data.altTxt
+    document.getElementsByClassName('item__img')[0].appendChild(img)    
+    document.getElementById('price').innerHTML = data.price
+    document.getElementById('description').innerHTML = data.description
+    for  (let i = 0 ; i < data.colors.length; i++) {
+        let option = document.createElement('option')
+        option.value = data.colors [i]
+        option.innerHTML = data.colors [i]
+        document.getElementById('colors').appendChild(option)
+        } 
+}
 )
-// Récupération des pièces depuis le fichier JSON
-const reponse = await fetch("http://localhost:3000/api/products/" + productId);
-const informationsArticle = await reponse.json();
-const informationsArticle = document.querySelector(".item");
-informationsArcticle.appendChild(img.src);
-informationsArticle.appendChild(img.alt);
-informationsArticle.appendChild(description.innerText);
-informationsArticle.appendChild(price.innerText);
-function products (name,price,description,altTxt,colors){
-    this.name = name;
-    this.price = price;
-    this.description = description;
-    this.altTxt = altTxt;
-    this.colors = colors;
-    }
-    let products =[];
-document.createElement('option')
-option.value = "sdds"
-option.innerHTML = "sadsasad"
-document.getElementById('leselect').appendChild(option)
+//ajouter au panier
+document.getElementById('addToCart').addEventListener('click',onAddToBasket)
+        function onAddToBasket(event) {
+                let item =
+                {
+                    id: '_id',
+                    color:'vert',
+                    quantity:1,
+                }
+            localStorage.setItem('Basketitems', JSON.stringify(item))
+        }
 
-<select>
-<option value="blue">Canape Bleu tres joli</option>
-<option value="pink">Canape Rose</option>
-</select>
-//travail du 17/01
-
-
-
-//
-//GamepadButton.addEventListener("click",function(){
-    //let color = colorInput.value;
-    //let quantityValue = parseInt(quantity.value);
-    //if (color === "" && quantityValue === 0){
-        //console.log(colorInput.value);
-        //alert("Vous devez sélectionner une couleur et une quantité")
-   // }
-    //stockage de l'id, qté,img et color dans le localStorage
-    //let productOrder ={
-     //   id: id,
-      //  quantity: quantityValue,
-      //  image: document.getElementById("productImage").src,
-      //  title: title.innerText,
-     //  / color: color,
-  //  };
-  //  let cart = localStorage.getItem("products");
-  //  if (cart == null) {
-      //  cart = [];
-  //  }else {
-   //     cart =JSON.parse(cart);
-  //  }
- // let newITem = true;
-    // if(....)si le produit existe déjà avec la même color il faut incrémenter la valeur
-    //produit.quantity += productOrder.quantity
- //   for (let produit of cart){
-     //   if {
-      //      productOrder.id === produit.id &&;
-      //      productOrder.color === produit.color;
-      //  }else {produit.quantity += productOrder.quantity;
-     //   newItem=false;}
-    //}
-   //if (newItem === true){
-  //  cart.push(productOrder);
- //  }     
- //  localStorage.setItem("products,JSON.stringify(cart))");
- //  alert("Le produit a été ajouté au panier");
- //   }
-//)
-
-
-
+        
 
 
 
