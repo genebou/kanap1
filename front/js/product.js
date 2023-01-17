128b45cedcbab26a46ced7592ede4d4695ca0543
Geneviève B <genevieve.bourchis@gmail.com>
2023-01-17 11:10:20 +0100

    tentative indication infos dans la page produits(titren couluers, description, alt)

--- front/js/product.js
+++ front/js/product.js
@@ -14,15 +14,28 @@ fetch("http://localhost:3000/api/products/" + productId)
 .then((res) => res.json())
 .then((data) => {
     console.log(data)
+    const article = pieces[0];
+
          let img = document.createElement('img')
         img.src = data.imageUrl
         img.alt = data.altTxt
         document.getElementsByClassName('item__img')[0].appendChild(img)
+        item_img.innerText = data.item_img
         document.getElementById('price').innerHTML = data.price
+        price.innerText = 'Prix :${data.price} €';
+
     }
 )
+function products (name,price,description,altTxt,colors){
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.altTxt = altTxt;
+    this.colors = colors;
+    }
+    let products =[];
 document.createElement('option')
-option.vaue = "sdds"
+option.value = "sdds"
 option.innerHTML = "sadsasad"
 document.getElementById('leselect').appendChild(option)
 
@@ -30,8 +43,49 @@ document.getElementById('leselect').appendChild(option)
 <option value="blue">Canape Bleu tres joli</option>
 <option value="pink">Canape Rose</option>
 </select>
-//test//
+//travail du 17/01
 
+
+
+//
+//GamepadButton.addEventListener("click",function(){
+    //let color = colorInput.value;
+    //let quantityValue = parseInt(quantity.value);
+    //if (color === "" && quantityValue === 0){
+        //console.log(colorInput.value);
+        //alert("Vous devez sélectionner une couleur et une quantité")
+   // }
+    //stockage de l'id, qté,img et color dans le localStorage
+    //let productOrder ={
+     //   id: id,
+      //  quantity: quantityValue,
+      //  image: document.getElementById("productImage").src,
+      //  title: title.innerText,
+       / color: color,
+  //  };
+  //  let cart = localStorage.getItem("products");
+  //  if (cart == null) {
+        cart = [];
+  //  }else {
+   //     cart =JSON.parse(cart);
+  //  }
+ // let newITem = true;
+    // if(....)si le produit existe déjà avec la même color il faut incrémenter la valeur
+    //produit.quantity += productOrder.quantity
+ //   for (let produit of cart){
+     //   if {
+      //      productOrder.id === produit.id &&;
+      //      productOrder.color === produit.color;
+      //  }else {produit.quantity += productOrder.quantity;
+     //   newItem=false;}
+    //}
+   //if (newItem === true){
+  //  cart.push(productOrder);
+ //  }     
+ //  localStorage.setItem("products,JSON.stringify(cart))");
+ //  alert("Le produit a été ajouté au panier");
+ //   }
+//)
 
 
 
