dcab7cd237f52fc7e13042e67c0a2d1c55bce0be
Geneviève B <genevieve.bourchis@gmail.com>
2023-01-24 16:34:50 +0100

    fiche produits

--- front/js/product.js
+++ front/js/product.js
@@ -1,58 +1,62 @@
-function getParams (url = window.location) {
-    // Create a params object
+function getParams(url = window.location) {
     let params = {};
     new URL(url).searchParams.forEach(function (val, key) {
         params[key] = val;
     });
     return params;
 }
-let params = getParams()
-console.log(params)
-let productId = params['id']
-console.log("Affichage du produit : " + productId)
-fetch("http://localhost:3000/api/products/" + productId)
-.then((res) => res.json())
-.then((data) => {
-    console.log(data)
-
-   let img = document.createElement('img')
-    img.src = data.imageUrl
-    img.alt = data.altTxt
-    document.getElementsByClassName('item__img')[0].appendChild(img)    
-    document.getElementById('price').innerHTML = data.price
-    document.getElementById('description').innerHTML = data.description
-    for  (let i = 0 ; i < data.colors.length; i++) {
-        let option = document.createElement('option')
-        option.value = data.colors [i]
-        option.innerHTML = data.colors [i]
-        document.getElementById('colors').appendChild(option)
-        } 
-}
-)
-//ajouter au panier
-document.getElementById('addToCart').addEventListener('click',onAddToBasket)
-        function onAddToBasket(event) {
-                let item =
-                {
-                    id: productId,
-                    color : "je ne sais pas comment faire",
-                    quantité: 8,
-                }
-            window.localStorage.setItem('Basketitems', JSON.stringify(item))
+function onAddToBasket(event) {
+    // let colorSelect = document.getElementById('colors')
+    const selectedColor = colors.options[colors.selectedIndex].value
+    // let quantity = document.getElementById('quantity')
+    // let selectedQuantity = quantity.value
+    let found = false
+    for (let i = 0 ; i < basketItems.length; i++) {
+        if (basketItems[i].id == productId && basketItems[i].color == selectedColor) {
+            basketItems[i].quantity = parseInt (basketItems[i].quantity ) + parseInt ( quantity.value )
+            found = true
+            break;
+        }        
+    }
+    if (!found) {        
+        let Kanap =
+        {
+            id: productId,
+            color: selectedColor,
+            quantity: quantity.value,
         }
-        //stockage de l'ID, quantité, image et couleur dans le localstorage
-        localStorage.getItem('Basketitems')
-                     
-           // let quantityValue = parseInt(quantity.value);
-           // if (color === "" && quantityValue === 0){
-             //   console.log(colorInput.value);
-            //    alert("Vous devez sélectionner une couleur et une quantité")
-          //  }
-           
-        
-     //   let productOrder ={
-    //  id: '_id',
-
-     //        };
-            
-        
+        basketItems.push(Kanap);
+    }
+    localStorage.setItem('Basketitems', JSON.stringify(basketItems))
+}
+var basketItems = JSON.parse(localStorage.getItem('Basketitems'));
+if (basketItems == null) {
+    basketItems = []
+}
+let params = getParams()
+var productId = params['id']
+/* if (typeof productId == "undefined" || productId == 0) {
+    window.location.href = "/404.html";
+} */
+// console.log("Affichage du produit : " + productId)
+fetch("http://localhost:3000/api/products/" + productId)
+    .then((res) => res.json())
+    .then((data) => {
+        // console.log(data);
+        document.title = data.name
+        let img = document.createElement('img')
+        img.src = data.imageUrl
+        img.alt = data.altTxt
+        document.getElementsByClassName('item__img')[0].appendChild(img)
+        title.innerHTML = data.name
+        price.innerHTML = data.price
+        description.innerHTML = data.description
+        for (let i = 0; i < data.colors.length; i++) {
+            let option = document.createElement('option')
+            option.value = data.colors[i]
+            option.innerHTML = data.colors[i]
+            document.getElementById('colors').appendChild(option)
+        }
+    }
+)
+document.getElementById('addToCart').addEventListener('click', onAddToBasket)
