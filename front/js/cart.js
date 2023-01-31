8583f5abc25eaa8fb83901874e1db3b3a5b35a28
Geneviève B <genevieve.bourchis@gmail.com>
2023-01-31 11:37:05 +0100

    début étape 7 cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,28 +1,22 @@
-fetch("http://localhost:3000/api/products")
-function getParams (url = window.location) {
-    // Create a params object
-    let params = {};
-    new URL(url).searchParams.forEach(function (val, key) {
-        params[key] = val;
-    });
-    return params;
+//récupérer l'array dans le localStorage
+var basketItems=localStorage.setItem('Basketitems', JSON.stringify(basketItems))
+if (basketItems == null) {
+    basketItems = []
 }
-let params = getParams()
-console.log(params)
-let productId = params['id']
-console.log("Affichage du panier")
-fetch("http://localhost:3000/api/products/")
-.then((res) => res.json())
-.then((data) => {
 
-//création HTML
-console.log(data)
+   // console.log(data)
 
-let article = document.createElement('Article')
-let cartItem= document.getElementsByClassName('cart_item')
 
-let cartItemImg = document.createElement('cart_item_img')
-document.getElementsByClassName('cart_item_img').appendChild(article)
 
-}
-)
+//parcourir l'array
+
+//let article = document.createElement('Article')
+//let idCartItem= document.getElementsByClassName('.cart_items')
+//CartItem.appenChild(article)
+//let cartItemImg = document.createElement('cart_item_img')
+//cartItem.appenChild(cartItemImg)
+//document.getElementsById('cart_items')
+
+
+
+
