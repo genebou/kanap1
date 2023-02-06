cdb51dab1a2804e4fafe4b22c49e15f54e8fd745
Geneviève B <genevieve.bourchis@gmail.com>
2023-02-06 17:39:34 +0100

    cart.js => jusqu'à la couleur ok HTML

--- front/js/cart.js
+++ front/js/cart.js
@@ -12,11 +12,47 @@ async function renderBasket(){
     let res  = await fetch('http://localhost:3000/api/products/'+ basketItems[i].id)
     let product = await res.json()
     console.log(product)
-    let cartItem = document.createElement('section')
-    cartItem.innerHTML= product.name
+    
+
+    let cartItem = document.createElement('article')
+    cartItem.innerHTML= product.colors
     document.getElementById('cart__items').appendChild(cartItem)
+    cartItem.classList.add('cart__item')
+    
+    let cartItemImg = document.createElement('div')
+    cartItemImg.classList.add('div')
+    cartItem.appendChild(cartItemImg)
+
+    let img = document.createElement('img')
+    cartItemImg.appendChild(img)
+    cartItemImg.classList.add('cart__item__img')
+    img.src=product.imageUrl
+    img.alt=product.altTxt
+
+    let cartItemContent = document.createElement('div')
+    cartItem.appendChild(cartItemContent)
+    cartItemContent.classList.add('cart__item__content')
+
+    let cartItemContentDescription = document.createElement('div')
+    cartItemContent.appendChild(cartItemContentDescription)
+    cartItemContentDescription.classList.add('cart__item__content__description')
+
+    let h2 = document.createElement('h2')
+    cartItemContentDescription.appendChild(h2)
+    h2.innerHTML=product.name
+    
+    
+      let color = document.createElement('p')
+        cartItemContentDescription.appendChild(color)
+        color.innerHTML = "vert"
+    }
+      
+    
+  
+   
+    
   }
-}
+
 renderBasket()
 
 
