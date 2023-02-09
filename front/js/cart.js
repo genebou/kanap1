22c3c80fd3ca40e3424fc52bbfbadbbd287cacad
Geneviève B <genevieve.bourchis@gmail.com>
2023-02-10 00:39:21 +0100

    attribute

--- front/js/cart.js
+++ front/js/cart.js
@@ -18,6 +18,8 @@ async function renderBasket(){
         cartItem.innerHTML= product.colors[i]
         document.getElementById('cart__items').appendChild(cartItem)
         cartItem.classList.add('cart__item')
+        cartItem.setAttribute("data-ID","product-ID")
+        cartItem.setAttribute("data-color","product-color")
       let cartItemImg = document.createElement('div')
         cartItemImg.classList.add('div')
         cartItem.appendChild(cartItemImg)
@@ -64,43 +66,83 @@ async function renderBasket(){
       let supprimer = document.createElement('p')
         deleteProduct.appendChild(supprimer)
         supprimer.classList.add('deleteItem')
-        deleteProduct.innerText="Supprimer"
+        supprimer.innerText="Supprimer"
 }
-}
-renderBasket()
 
+
+//création d'une fonction qui ve mettre à jour le basketItem 
+//afin d'envoyer un panier actualisé dans la page confirmation
 function deleteBasketItems(){
-    basketDelete =[]
-    let deleteBtn = document.querySelectorAll("deleteItem");
-    
-    for (let i=deleteBtn.length -1; i>=0;i--){
-      deleteBtn.addEventlistener("click", ()=>{
-        if (deleteBtn[i].style.display = "none"){
-          basketDelete = basketItems;
-       } else{
-        for (let i=0; i<basketDelete.length; i++) {
-         // delete basketDelete[i].altTxt;
-          //delete basketDelete [i].imageUrl;
-         // delete basketDelete[i].name;
-         // delete basketDelete[i].price;
-          basketDelete.splice([i]);
+  //document.querySelectorAll('itemQuantity').innerHTML="";
+   document.querySelectorAll('deleteItem').addEventListener('click', function(event) {
+    let deleteBtn = event.target
+    let itemDiv = deleteBtn.parentNode.parentNode.parentNode // pour remonter dans l'arborescence HTML du bouton vers la div
+    let itemId = itemDiv.dataset.id 
+  console.log(deleteBtn)
+  let basketDelete =[]
+  console.log(basketDelete)
+  console.log(basketItems)
+      for (let i=0; i< deleteBtn.length ;i++){
+      deleteBtn[i].addEventlistener("click", function (){ 
+        deleteBtn[i].style.display="none";
+        basketDelete = basketItems;
+      for (let i= basketDelete.length -1; i > 0; i--) {
+        delete basketDelete[i].altTxt;
+        delete basketDelete[i].imageUrl;
+        delete basketDelete[i].name;
+        delete basketDelete[i].price;
+
+        basketDelete= basketItems.splice([i],1);
+          console.log(basketDelete)
+          alert(ok)
          }
-       }
-       console.log(basketDelete)
-     }
-    )
-  }
+        }
+      )
+      
+    }
+  })
 }
 deleteBasketItems()
-console.log(deleteBasketItems)
+}
 
 
 
+renderBasket()
 
-  
+
+
+ 
+   /* //à chaque click, le basketDelete sera actualisé -basketItems actualisé)
+    let deleteBtn = document.querySelectorAll('cart__item__content__setting__delete');
+      for (let i=0; i<deleteBtn.length;i++){
+      
+        for(i=0; i<deleteBtn.length; i++){
+           deleleBtn.addEventlistener("click", function (){       
+       for (i=0; i<deleteBtn.length; i++){
+       if( 
+          deleteBtn[i].style.display = "none"
+           )
+        {basketDelete = basketItems [i]
+          alert(1)
+        }
+        else(
+           basketDelete.splice([i,1])
+        )
+        } 
+        console.log(basketDelete)
+       }
+       )
+       
+      }
+      
+    }
     
+  } 
+    
+ 
 
+deleteBasketItems()
        
                
        
-      
+     */
